d2648c2072046c4a22e9b5f7a8acc5a5ec87be09
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-11-14 15:14:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import requests
 url = ‘http://weather.livedoor.com/forecast/webservice/json/v1’ 
 payload = {‘city’:’130010′} 
 tenki_data = requests.get(url, params=payload).json()
-
-
+location = tenki_data[‘title’]
+date = tenki_data[‘forecasts’][0][‘date’]
+forecasts = tenki_data[‘forecasts’][0][‘telop’] 
+temp_max = tenki_data[‘forecasts’][0][‘temperature’][‘max’][‘celsius’]
+temp_min = tenki_data[‘forecasts’][0][‘temperature’][‘min’][‘celsius’]
+info_time = tenki_data[‘publicTime’]
+                  
 browser = webdriver.PhantomJS() 
 loginURL = "http://example.com/wp/wp-login.php" #yoursite.comにはあなたのサイトのアドレスを書いてください
 browser.get(loginURL)
@@ -14,7 +19,6 @@ time.sleep(3)
 print(browser.title) # "あなたのサイト名 ‹ ログイン"と表示される
 
 #ログイン
-
 username = ""　#ユーザー名を入れてください
 password = "" #パスワードを入れてください
 
@@ -30,14 +34,8 @@ browser.get("http://yoursite.com/wp/wp-admin/post-new.php")
 time.sleep(3) # waiting for sign in 
 print(browser.title) #"新規投稿を追加 ‹ あなたのサイト名 — WordPress"と表示される
 
-location = tenki_data[‘title’]
-date = tenki_data[‘forecasts’][0][‘date’]
-forecasts = (tenki_data[‘forecasts’][0][‘telop’] 
-temp_max = [‘forecasts’][0][‘temperature’][‘max’][‘celsius’]
-info_time = print(tenki_data[‘publicTime’]
-
 title = "今日の" + location
-content = "今日の" + location + "は、<br>" + 
+content = "今日の" + location + "は、" + forecasts + "です。<br>" + "最高気温は、" + temp_max + "℃で、最低気温は" + temp_min + "℃です。<br>" + "このデータは" + info_time + "時点の情報です。"
 browser.find_element_by_css_selector("#title").send_keys(title)
 browser.find_element_by_css_selector("#content_ifr").send_keys(content)
 browser.find_element_by_css_selector("#save-post").click
